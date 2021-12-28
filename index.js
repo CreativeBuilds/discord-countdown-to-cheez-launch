79cb57196c14a1bbe75e1f50ec16053302677a4b
CreativeBuilds <nick@creativebuilds.io>
2021-12-27 19:00:39 -0500

    adds dotenv

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const timeUntil = require('time-until')
+const timeUntil = require('time-until');
+
 
 client.on('ready', () => {
     setInterval(() => {
         gameReleaseDate = new Date(2021, 12, 30, 12, 42, 42);
+
         // if gamereleasedate has passed, set status to offline
         if (gameReleaseDate < Date.now()) {
             client.user.setStatus('invisible');
